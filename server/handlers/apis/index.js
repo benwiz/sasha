e19b9353fe5f7a6abd04d72493653724b927d13b
Ben Wisialowski <ben@wiz3.com>
2017-02-12 16:36:14 -0600

    set up basic handler. not sure if this is the right approach

--- server/handlers/apis/index.js
+++ server/handlers/apis/index.js
@@ -1,13 +1,30 @@
 'use strict';
 
+// external libs
+const Request = require('request');
+// internal libs
+const IntentHandler = require('./lib/intent-handler');
+
+const sasha_api_url = 'http://localhost:8080';
 
 const text = (request, reply) => {
 
-    reply('text');
+    // post to sasha-api /nlp
+    Request.post({
+        url: sasha_api_url + '/nlp',
+        body: JSON.stringify(request.payload)
+    }, (error, response, body) => {
+
+        const handler = IntentHandler.selector(body.intent);
+        console.log(handler);
+        reply(body);
+    });
 };
 
 const audio = (request, reply) => {
 
+    // post to sasha-api /speech-to-text
+    // then utilize same logic as text function above
     reply('audio');
 };
 
