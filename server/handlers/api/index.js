0fd51de7f8d9d1077fdf401217a75984747a6efd
Ben Wisialowski <ben@wiz3.com>
2017-03-12 13:45:59 -0500

    update mopidy host works

--- server/handlers/api/index.js
+++ server/handlers/api/index.js
@@ -12,6 +12,7 @@ const config = (request, reply) => {
     if (payload.mopidy_host) {
         process.env.MOPIDY_HOST = payload.mopidy_host;
         Mopidy.mopidy._settings.webSocketUrl = `ws://${payload.mopidy_host}:6680/mopidy/ws/`;
+        Mopidy.mopidy.connect();
     }
     if (payload.watson_username) process.env.WATSON_USERNAME = SpeechToText.WatsonSTT._options.username = payload.watson_username;
     if (payload.watson_password) process.env.WATSON_USERNAME = SpeechToText.WatsonSTT._options.password = payload.watson_password;
