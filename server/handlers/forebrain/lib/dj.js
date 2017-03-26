c35ecd2346da4a8febcefb7d69a7488bcbe067fc
Ben Wisialowski <ben@wiz3.com>
2017-03-26 00:55:12 -0500

    notes

--- server/handlers/forebrain/lib/dj.js
+++ server/handlers/forebrain/lib/dj.js
@@ -1,10 +1,15 @@
 'use strict';
 
+// https://datasciencelab.wordpress.com/2013/12/12/clustering-with-k-means-in-python/
+// https://keras.io/getting-started/sequential-model-guide/
+
 const start = () => {
 
     console.log('hello, it\'s me the dj');
 
     return new Promise((resolve, reject) => {
+
+        // NOTE: this tool might need to be a wrapper around a python tool
 
         resolve('abc');
     });
