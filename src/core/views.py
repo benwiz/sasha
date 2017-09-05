91a2eee4eb17304a49bff41badbd5f871048688d
Ben Wisialowski <bwisialowski@gmail.com>
2017-09-04 19:36:15 -0500

    notes

--- src/core/views.py
+++ src/core/views.py
@@ -24,5 +24,9 @@ def index(request):
     return HttpResponse(response, content_type='application/json', status=200)
 
 
+# Start listeneing to Facebook threads in another process. If I start adding
+# more and more services I should figure out a better way to run parallel
+# processes in Django or consider separating out these jobs into another Docker
+# container.
 p = Process(target=fb.listen)
 p.start()
