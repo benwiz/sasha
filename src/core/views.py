4b9b046c079f92b1cfd3e41a064642f1cfd51cda
Ben Wiz <bwisialowski@gmail.com>
2017-09-23 10:23:03 -0500

    remove old code

--- src/core/views.py
+++ src/core/views.py
@@ -7,8 +7,6 @@ As views start to collect, they should be broken into smaller applications.
 import json
 from multiprocessing import Process
 
-from . import fb
-
 from django.shortcuts import render, HttpResponse
 
 
@@ -22,12 +20,3 @@ def index(request):
     }
     response = json.dumps(response)
     return HttpResponse(response, content_type='application/json', status=200)
-
-# Start listeneing to Facebook threads in another process. If I start adding
-# more and more services I should figure out a better way to run parallel
-# processes in Django or consider separating out these jobs into another Docker
-# container.
-listener_proc = Process(target=fb.listen)
-listener_proc.start()
-# reminder_proc = Process(target=fb.cron)
-# reminder_proc.start()
