cb0b534ef4869628aee2de5cb0b7eaac2a164600
Ben Wiz <bwisialowski@gmail.com>
2017-09-23 11:33:14 -0500

    get states endpoint

--- src/core/urls.py
+++ src/core/urls.py
@@ -7,5 +7,6 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    url(r'^state', views.state, name='state'),
     url(r'^$', views.status, name='status'),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -9,8 +9,13 @@ Probably should not use this application for any front-end work.
 
 import json
 from multiprocessing import Process
+from pprint import pprint
 
 from django.shortcuts import render, HttpResponse
+from core.worldstate import WorldState
+
+current_state = WorldState('current')
+desired_state = WorldState('desired')
 
 
 def status(request):
@@ -23,3 +28,31 @@ def status(request):
     }
     response = json.dumps(response)
     return HttpResponse(response, content_type='application/json', status=200)
+
+
+def state(request):
+    """
+    Get the states.
+    """
+
+    if request.method == 'GET':
+
+        response = {
+            'current_state': current_state.get_state(),
+            'desired_state': desired_state.get_state(),
+            'commands': current_state.get_commands(desired_state)
+        }
+        response = json.dumps(response)
+        return HttpResponse(response,
+                            content_type='application/json',
+                            status=200)
+
+    else:
+        response = {
+            'status': 405,
+            'message': 'Method %s not allowed.' % request.method
+        }
+        response = json.dumps(response)
+        return HttpResponse(response,
+                            content_type='application/json',
+                            status=405)
