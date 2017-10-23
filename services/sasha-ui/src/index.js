12d7998ac83aa8e817ef00e8faea037a96a9bccd
Ben Wisialowski <bwisialowski@gmail.com>
2017-10-22 19:22:55 -0500

    implement my own custom templating

--- services/sasha-ui/src/index.js
+++ services/sasha-ui/src/index.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
 
+const replaceTemplates = (replacements, html) => {
+  let result = html;
+  Object.keys(replacements).forEach((key) => {
+    result = result.replace(`{{ ${key} }}`, replacements[key]);
+  });
+  return result;
+};
+
 exports.handler = (event, context) => {
   fs.readFile('index.html', 'utf-8', (err, data) => {
-    context.succeed(data);
+    const replacements = {
+      current_state: { salt_lamp: { state: 'on' } },
+      desired_state: { salt_lamp: { state: 'on' } },
+    };
+    const html = replaceTemplates(replacements, data);
+    context.succeed(html);
   });
 };
