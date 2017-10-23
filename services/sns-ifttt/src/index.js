803555f68834c214a527d441d11098fa203c4de8
Ben Wisialowski <bwisialowski@gmail.com>
2017-10-22 20:22:28 -0500

    receive messgae

--- services/sns-ifttt/src/index.js
+++ services/sns-ifttt/src/index.js
@@ -1,9 +1,13 @@
 const Https = require('https');
 const Querystring = require('querystring');
 
+const iftttSecretKey = process.env.IFTTT_SECRET_KEY;
+// 'https://maker.ifttt.com/trigger/' + iftttMakerEventName + '/with/key/' + iftttSecretKey
+
 // Display the contents of the index.html file
 const handler = (event, context) => {
-  context.succeed('yay!');
+  const message = event.Records[0].Sns.Message;
+  console.log('From SNS:', message);
 };
 
 exports.handler = handler;
