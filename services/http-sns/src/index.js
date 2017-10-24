2facbc85d55832c98e9e0ab9dc3516dc9181404a
Ben Wisialowski <bwisialowski@gmail.com>
2017-10-23 19:24:05 -0500

    it works!

--- services/http-sns/src/index.js
+++ services/http-sns/src/index.js
@@ -1,4 +1,4 @@
-const AWS = require('aws-sdk');
+const SnsPublish = require('aws-sns-publish');
 
 exports.handler = (event, context, callback) => {
   /*
@@ -21,32 +21,11 @@ exports.handler = (event, context, callback) => {
   }
   */
 
-  AWS.config.update({
-    // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-1',
-  });
-
-  const sns = new AWS.SNS();
-  const params = {
-    Message: 'wemo_light_1_off', /* required */
-    // MessageAttributes: {
-    //   '<String>': {
-    //     DataType: 'STRING_VALUE', /* required */
-    //     BinaryValue: new Buffer('...') || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */,
-    //     StringValue: 'STRING_VALUE'
-    //   },
-    //   /* '<String>': ... */
-    // },
-    // MessageStructure: 'STRING_VALUE',
-    // PhoneNumber: 'STRING_VALUE',
-    // Subject: 'STRING_VALUE',
-    // TargetArn: 'STRING_VALUE',
-    // TopicArn: 'STRING_VALUE',
-  };
-  sns.publish(params, (err, data) => {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
+  const params = { arn: 'arn:aws:sns:us-east-1:778257796245:ifttt' };
+  const data = { action: 'wemo_switch_1_off' };
+  const content = JSON.stringify(data);
+  SnsPublish(content, params).then((messageId) => {
+    console.log('messageId:', messageId);
   });
 
   const response = {
