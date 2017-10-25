ec73a84d93549cd3ab5078f5c1fdaf934b96aa3d
Ben Wisialowski <bwisialowski@gmail.com>
2017-10-24 21:07:00 -0500

    text-bunny

---
+++ services/text-bunny/src/index.js
@@ -0,0 +1,22 @@
+const SnsPublish = require('aws-sns-publish');
+
+exports.handler = (event, context, callback) => {
+
+  if (!process.env.PHONE) {
+    return callback('Missing environment variable for PHONE.');
+  }
+
+  const message = '🐰🐰🐰🐰🐰🐰🐰🐰🐰🐰🐰\n🐰 Good morning! 🐰\n🐰🐰🐰🐰🐰🐰🐰🐰🐰🐰🐰';
+  const params = { phone: process.env.PHONE };
+  SnsPublish(message, params)
+    .then((messageId) => {
+      console.log('messageId:', messageId);
+      callback(null, { message: 'success' });
+    })
+    .catch((err) => {
+      console.log(err);
+      callback(null, { message: 'success' });
+    });
+
+  return 0;
+};
