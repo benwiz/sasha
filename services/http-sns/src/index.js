9d66d72339f41c3af1c5a7907e6bef94c3777920
Ben Wisialowski <bwisialowski@gmail.com>
2017-10-24 19:51:09 -0500

    send text

--- services/http-sns/src/index.js
+++ services/http-sns/src/index.js
@@ -19,31 +19,33 @@ exports.handler = (event, context, callback) => {
     return callback(null, response);
   }
 
-  const data = body;
-  const params = { arn: `arn:aws:sns:us-east-1:778257796245:${queryString.topic}` };
-  console.log(data, params);
-  SnsPublish(data, params)
-    .then((messageId) => {
-      console.log('messageId:', messageId);
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({ messageId }),
-      };
-      callback(null, response);
-    })
-    .catch((err) => {
-      console.log(err);
-      const response = {
-        statusCode: 500,
-        body: JSON.stringify(err),
-      };
-      callback(null, response);
-    });
-
-  // snsPublish('SMS Message', {phone: '+14155552671'})
-  //  .then(messageId => {
-  //     console.log(messageId);
-  //  });
+  if (queryString.topic.lower() === 'sms') {
+    const data = body;
+    const params = { arn: `arn:aws:sns:us-east-1:778257796245:${queryString.topic}` };
+    console.log(data, params);
+    SnsPublish(data, params)
+      .then((messageId) => {
+        console.log('messageId:', messageId);
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify({ messageId }),
+        };
+        callback(null, response);
+      })
+      .catch((err) => {
+        console.log(err);
+        const response = {
+          statusCode: 500,
+          body: JSON.stringify(err),
+        };
+        callback(null, response);
+      });
+  } else {
+    SnsPublish(body.message, { phone: body.phone })
+      .then((messageId) => {
+        console.log(messageId);
+      });
+  }
 
   return 0;
 };
