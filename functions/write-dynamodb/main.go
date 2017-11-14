034664ea0bc398271641b4e84b475f2b5a4c54c0
Ben Wisialowski <bwisialowski@gmail.com>
2017-11-14 08:07:04 -0600

    notes

--- functions/write-dynamodb/main.go
+++ functions/write-dynamodb/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/apex/go-apex"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"os"
 )
 
@@ -14,8 +15,11 @@ type message struct {
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
 		fmt.Fprintf(os.Stderr, "Event: %s", event)
-		var m message
 
+		// TODO: Instead of unmarshalling the event into the message, just write
+		// it directly to DynamoDB.
+
+		var m message
 		if err := json.Unmarshal(event, &m); err != nil {
 			return nil, err
 		}
