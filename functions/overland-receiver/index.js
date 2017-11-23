10c7c35d8ced1eff14a63b5f7af718898bdf9e2d
Ben Wisialowski <bwisialowski@gmail.com>
2017-11-23 09:59:52 -0500

    correctly handle ifttt

--- functions/overland-receiver/index.js
+++ functions/overland-receiver/index.js
@@ -3,7 +3,6 @@ const Request = require('request');
 
 
 const iftttSecretKey = process.env.IFTTT_SECRET_KEY;
-const phoneNumber = process.env.PHONE_NUMBER;
 
 const getIFTTTWebhook = (action, payload) => new Promise((resolve, reject) => {
   const data = JSON.stringify(payload);
@@ -15,25 +14,27 @@ const getIFTTTWebhook = (action, payload) => new Promise((resolve, reject) => {
     if (error) {
       return reject(error);
     }
-    console.log(body);
+    if (body !== 'Congratulations! You\'ve fired the record_overland_data event') {
+      body = JSON.parse(body);
+    }
     return resolve(body);
   });
 });
 
-const sendSNS = (topic, payload) => new Promise((resolve, reject) => {
-  const data = JSON.stringify(payload);
-  Request.post({
-    headers: { 'content-type': 'application/json' },
-    url: `https://sasha.benwiz.io/sns?topic=${topic}`,
-    body: data,
-  }, (error, response, body) => {
-    if (error) {
-      return reject(error);
-    }
-    const obj = JSON.parse(body);
-    return resolve(obj);
-  });
-});
+// const sendSNS = (topic, payload) => new Promise((resolve, reject) => {
+//   const data = JSON.stringify(payload);
+//   Request.post({
+//     headers: { 'content-type': 'application/json' },
+//     url: `https://sasha.benwiz.io/sns?topic=${topic}`,
+//     body: data,
+//   }, (error, response, body) => {
+//     if (error) {
+//       return reject(error);
+//     }
+//     const obj = JSON.parse(body);
+//     return resolve(obj);
+//   });
+// });
 
 const getLatestLocation = (data) => {
   const latestData = data.locations[data.locations.length - 1];
@@ -57,39 +58,34 @@ exports.handle = (event, context, callback) => {
     return callback(null, response);
   }
 
-  const data = JSON.parse(event.body);
+  // const data = JSON.parse(event.body);
   // const coords = getLatestLocation(data);
 
-  // Send data to IFTTT -> Google Spreadsheet.
-  // Currently we are just packaing the entirety of payload into a JSON string and storing that.
-  // It will need to be extracted and parsed at a later date. This function may change over time.
-  Promise.resolve() // TODO: Write coords to database (DynamoDB, either directly or via api)
+  Promise.resolve()
+    // Send data to IFTTT -> Google Spreadsheet.
     .then(() => {
       const action = 'record_overland_data';
-      const payload = { value1: event.body };
+      const payload = { value1: event.body.replace(/\n/g, '').replace(/ /g, '') };
       return getIFTTTWebhook(action, payload);
     })
-    // .then(() => {
-    //   const snsPayload = {
-    //     phone: phoneNumber,
-    //     message: `Latitude: ${coords.latitude}\n` +
-    //              `Longitude: ${coords.longitude}\n` +
-    //              `Timestamp: ${coords.timestamp}`,
-    //   };
-    //   console.log('Send SMS:', JSON.stringify(snsPayload));
-    //   return sendSNS('sms', snsPayload);
-    // })
-    .then(() => {
-      const reply = {
-        statusCode: 200,
-        body: JSON.stringify({ result: 'ok' }), // This is specifically what the Overland app expects (https://github.com/aaronpk/Overland-iOS/blob/e192244a76f3bcb1f495a3aee9cde816ca63de3d/GPSLogger/GLManager.m#L160)
-      };
+    // HTTP Response
+    .then((res) => {
+      console.log('IFTTT RES:', res, typeof res);
+      const reply = {};
+      // If IFTTT responded with errors
+      if (res.errors) {
+        reply.statusCode = 500;
+        reply.body = JSON.stringify({ message: 'IFTTT errors.', errors: res.errors });
+      } else {
+        reply.statusCode = 200;
+        reply.body = JSON.stringify({ result: 'ok' }); // This is specifically what the Overland app expects (https://github.com/aaronpk/Overland-iOS/blob/e192244a76f3bcb1f495a3aee9cde816ca63de3d/GPSLogger/GLManager.m#L160)
+      }
       callback(null, reply);
     })
     .catch((err) => {
       const reply = {
         statusCode: 500,
-        body: JSON.stringify({ message: 'Bad response from IFTTT.', error: err }),
+        body: JSON.stringify({ message: 'Error.', error: err }),
       };
       callback(null, reply);
     });
