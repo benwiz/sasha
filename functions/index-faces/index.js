7a59e3118b905ad360dc7a682d8ee67b5f9debd0
Ben Wisialowski <bwisialowski@gmail.com>
2017-11-28 18:15:55 -0800

    index faces first attempt

--- functions/index-faces/index.js
+++ functions/index-faces/index.js
@@ -2,12 +2,39 @@ const Promise = require('bluebird');
 const Request = require('request');
 const AWS = require('aws-sdk');
 
+const rekognition = new AWS.Rekognition();
+
+const indexFaces = () => new Promise((resolve, reject) => {
+  const params = {
+    CollectionId: 'faces',
+    DetectionAttributes: [
+    ],
+    Image: {
+      S3Object: {
+        Bucket: 'sasha-faces',
+        Name: 'ben.jpg',
+      },
+    },
+  };
+  rekognition.indexFaces(params, (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(data);
+    }
+  });
+});
+
 exports.handle = (event, context, callback) => {
   console.log('EVENT:', event);
 
-  const rek = new AWS.Rekognition();
-  console.log('rek:', rek);
-
+  indexFaces()
+    .then((res) => {
+      console.log('Result:', res);
+    })
+    .catch((err) => {
+      console.log('Error:', err);
+    });
   const reply = { message: 'ROFL.' };
   return callback(null, reply);
 };
