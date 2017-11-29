151e3c70a82bb1c18f97aea7642cd15bd70d8ab4
Ben Wisialowski <bwisialowski@gmail.com>
2017-11-28 17:31:38 -0800

    analyze-image

--- functions/analyze-image/index.js
+++ functions/analyze-image/index.js
@@ -4,18 +4,7 @@ const Request = require('request');
 
 exports.handle = (event, context, callback) => {
   console.log('EVENT:', event);
-  console.log('BODY:', event.body);
 
-  if (!event.body) {
-    const response = {
-      statusCode: 400,
-      body: JSON.stringify({ message: 'Missing payload!' }),
-    };
-    return callback(null, response);
-  }
-  const reply = {
-    statusCode: 500,
-    body: JSON.stringify({ message: 'LOL.' }),
-  };
+  const reply = { message: 'LOL.' };
   return callback(null, reply);
 };
