f986f5e525c42f6982c8d8cde4171c171ca99934
Ben Wisialowski <bwisialowski@gmail.com>
2017-12-16 10:37:14 -0600

    get locations data

--- functions/util_geofencer/main.go
+++ functions/util_geofencer/main.go
@@ -4,8 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/apex/go-apex"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
+
+var sashaURL = "https://sasha.benwiz.io"
 
 type message struct {
 	QueryStringParameters queryStringParameters `json:"queryStringParameters"`
@@ -43,6 +47,43 @@ func main() {
 			return r, nil
 		}
 		fmt.Fprintf(os.Stderr, "Message: %s\n", m)
+
+		// Get lat and lng from message
+		lat := m.QueryStringParameters.Lat
+		lng := m.QueryStringParameters.Lng
+		fmt.Fprintf(os.Stderr, "Lat: %f\n", lat)
+		fmt.Fprintf(os.Stderr, "Lng: %f\n", lng)
+
+		// GET {sashaURL}/dynamo/locations
+		resp, err := http.Get(sashaURL + "/dynamo/locations")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Get /dynamo/locations error: %s\n", err)
+			r.StatusCode = 500
+			r.Body = fmt.Sprintf(`{"message": "%s"}`, err)
+			return r, nil
+		}
+		defer resp.Body.Close()
+
+		// Read response
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Read /dynamo/locations response error: %s\n", err)
+			r.StatusCode = 500
+			r.Body = fmt.Sprintf(`{"message": "%s"}`, err)
+			return r, nil
+		}
+		fmt.Fprintf(os.Stderr, "Locations body:%s\n", string(body))
+
+		// Parse into location structs
+		locations := make([]location, 0)
+		err = json.Unmarshal(body, &locations)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Get /dynamo/locations error: %s\n", err)
+			r.StatusCode = 500
+			r.Body = fmt.Sprintf(`{"message": "%s"}`, err)
+			return r, nil
+		}
+		fmt.Fprintf(os.Stderr, "Locations slice:%s\n", locations)
 
 		// Respond
 		r.StatusCode = 200
